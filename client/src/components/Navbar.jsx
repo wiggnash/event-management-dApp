baff80eb45ef4e98d9c08a24b7fc8a40037d6acf
vignesh <vigneshwar.sigamani@gmail.com>
2023-08-03 13:50:23 +0530

    add navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,34 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import { useState } from 'react';
+
+import logo from '../../images/logo.png';
+
+const NavbarItem = ({title,classProps}) => {
+    return(
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    )
+}
+
 const Navbar = () => {
+    const [toogleMenu,setToggleMenu] = useState(false);
+
     return (
-        <h1>Navbar</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
+            <ul className = "text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+                {["Browse Events","Showcase","Product","Company"].map((item,index) => (
+                    <NavbarItem key={item+index} title={item} />
+                ))} 
+                <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                    Login
+                </li>
+            </ul>
+        </nav>
     );
 }
 
